8811d83c0ae8dd2de34399b06713aa16cd3fe307
Michel Caluwaerts m.calu@skynet.be <m.calu@skynet.be>
2022-04-02 13:36:36 +0200

    routes accounts and rates

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "my_accounts", to: "pages#my_accounts",  as: :account_list
+  
+  resources :accounts
+  resources :rates
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
