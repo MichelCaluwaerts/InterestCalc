5dc962d33a536bb3a19605c3383b6461b66bb382
Michel Caluwaerts m.calu@skynet.be <m.calu@skynet.be>
2022-06-25 16:51:34 +0200

    flatpickr de base

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,9 +3,7 @@ import { French } from "flatpickr/dist/l10n/fr.js"
 
 const initFlatpickr = () => {
   flatpickr(".datepicker", { 
-    "locale": French,
-    altInput: true,
-    altFormat: "j-m-Y",
+    "locale": French
 });
 }
 
