c5d767126092ef53bab0098e58933fe43563e3d9
Michel Caluwaerts m.calu@skynet.be <m.calu@skynet.be>
2022-07-19 13:42:41 +0200

    i18n in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
   before_action :authenticate_user!
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 end
